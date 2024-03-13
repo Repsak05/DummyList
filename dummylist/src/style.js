58e8539f31d83802a23c82a926ed9497ada16dfa
NordinP22R <49874265+nordinmp@users.noreply.github.com>
2024-03-13 15:24:09 +0100

    Colors work

--- dummylist/src/style.js
+++ dummylist/src/style.js
@@ -1,16 +1,25 @@
 // styles.js
 import { StyleSheet } from 'react-native';
+import colors, { lightColors, darkColors } from './colors.js';
 
 export default StyleSheet.create({
  container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: colors.lightColors.Background,
+    
  },
  text: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    borderWidth: 5,
+
  },
+
+ carouselItem: {
+    width: 408, 
+    height: 204, 
+ }
 });
