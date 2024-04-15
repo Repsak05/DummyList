7d6d7dd2b5d11a0e995c6c0164281c4a82b29b3b
KasperL22R <119696654+Repsak05@users.noreply.github.com>
2024-04-15 17:21:57 +0200

    Feature: Added Share post / delete post buttons (Missing; Buttons not currently working))

--- dummylist/src/screens/CalendarPage.js
+++ dummylist/src/screens/CalendarPage.js
@@ -8,10 +8,15 @@ import FeedLikedBy from "../components/FeedLikedBy.js";
 import { readData } from "../../firebase.js";
 
 export default function CalendarPage({navigation}) 
-{ //TODO: Create post settings  (overlay?)
+{ //TODO: Finish post settings
 
     const [onCurrentPost, setOnCurrentPost] = useState(0);
     const [allPosts, setAllPosts] = useState([])
+
+    const [isEditTabOpen, setIsEditTabOpen] = useState(false)
+    const heightOfOverlay = 200;
+
+    const ifTooManyPostsChangeMargin = allPosts.length >= 20 ? 4 : 10 
 
     useEffect(() => {
         async function getAllPosts()
@@ -67,22 +72,35 @@ export default function CalendarPage({navigation})
                             <ImageBackground source={{uri: allPosts[onCurrentPost].PostUri}} style={[style.roundedCorners, {width: 410, height: 727, position: "relative", borderRadius: "50%"}]}>
                                 <View style={{flexDirection: "row", width: "100%", alignContent: "center", position: "relative", top: 30, left: 0, marginHorizontal: 10}}>
                                     { allPosts.map((post, index) => (
-                                        <View key={index} style={{width: ((410/(allPosts.length)) - 10 - (10/allPosts.length)), marginRight: 10, height: 3, backgroundColor: index == onCurrentPost ? "#FFF8F7" : "#c4c4c4"}}></View>
+                                        <View key={index} style={{width: ((410/(allPosts.length)) - ifTooManyPostsChangeMargin - (20/allPosts.length)), marginRight: ifTooManyPostsChangeMargin, height: 3, backgroundColor: index == onCurrentPost ? "#FFF8F7" : "#c4c4c4"}}></View>
                                     ))}
                                 </View>
                             </ImageBackground>
                         </View>
                         <View style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: '50%'}}>
-                            <Pressable onPress={previousPost} style={{flex: 1}} />
+                            <Pressable onPress={() => {previousPost(); setIsEditTabOpen(false);}} style={{flex: 1}} />
                         </View>
                         <View style={{position: 'absolute', top: 0, bottom: 0, right: 0, left: '50%'}}>
-                            <Pressable onPress={nextPost} style={{flex: 1}} />
+                            <Pressable onPress={() => {nextPost(); setIsEditTabOpen(false);}} style={{flex: 1}} />
                         </View>
                     </View>
                     
                     <View style={{justifyContent: "space-between", flexDirection: "row", marginTop: 10}}>
+                        {isEditTabOpen && (
+                            <View style={[{height: heightOfOverlay, width: 150, borderRadius: 10, backgroundColor: "#d9d9d9", position: "absolute", top: -(heightOfOverlay+0), right: 20, flexDirection: "column", padding: 10}]}>
+                                <Pressable onPress={() => {console.log("Clicked: Missing function; Share")}} style={{marginBottom: 10}}>
+                                    <Text style={style.blackFontSize16}>Share</Text>
+                                </Pressable>
+                                <Pressable onPress={() => {console.log("Clicked: Missing function; Delete")}} style={{marginBottom: 10}}>
+                                    <Text style={style.blackFontSize16}>Delete</Text>
+                                </Pressable>
+                                <Pressable style={{position: "absolute", top: 10, right: 10}} onPress={(() => setIsEditTabOpen(false))}>
+                                    <Text style={style.blackFontSize16}>X</Text>
+                                </Pressable>
+                            </View> 
+                        )}
                         <Text style={[style.blackFontSize16Medium, {marginLeft: 19, paddingHorizontal: 5}]}>"{allPosts[onCurrentPost].TaskDescription}"</Text>
-                        <Pressable onPress={() => {console.log("Open edit tap")}}>
+                        <Pressable onPress={() => {console.log("Open edit tap"); setIsEditTabOpen(!isEditTabOpen);}}>
                             <Image style={{marginRight: 19}} source={require("../assets/icons/moreOptionsIcon.svg")}/>
                         </Pressable>
                     </View>
